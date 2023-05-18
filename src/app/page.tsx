568047ef7062c1c8222fd5a24a925da42621ed56
JeremyDevCode <jeremydevcode@gmail.com>
2023-05-17 23:06:41 -0500

    feat: print section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
       <h1 className="text-8xl font-extrabold">
         Learning <span className="text-sky-400">C</span>
       </h1>
-      <main className="flex flex-col w-full gap-20">
-        <section className="flex flex-col gap-8 w-full relative">
-          <div className="flex gap-8">
+      <main className="flex flex-col w-full gap-16">
+        <section className="flex flex-col gap-8 w-[36%] relative">
+          <div className="flex gap-8 w-full justify-between">
             <h2 className="text-4xl">Declaración</h2>
-            <button className="bg-[#FFFFFF11] p-2 rounded-md">
+            <button className="bg-[#FFFFFF11] text-[#999] hover:text-[#E3E6E8] p-2 rounded-md transition-colors hover:bg-[#FFFFFF15] ">
               <ArrowIcon />
             </button>
           </div>
@@ -34,10 +34,10 @@ export default function Home() {
             b;
           </p>
         </section>
-        <section className="flex flex-col gap-8 w-full relative">
-          <div className="flex gap-8">
+        <section className="flex flex-col gap-8 w-[36%] relative">
+          <div className="flex gap-8 justify-between">
             <h2 className="text-4xl">Inicialización</h2>
-            <button className="bg-[#FFFFFF11] p-2 rounded-md">
+            <button className="bg-[#FFFFFF11] text-[#999] hover:text-[#E3E6E8] p-2 rounded-md hover:bg-[#FFFFFF15]  transition-colors">
               <ArrowIcon />
             </button>
           </div>
@@ -58,6 +58,64 @@ export default function Home() {
               </span>
             </span>{" "}
             = 7.5;
+          </p>
+        </section>
+        <section className="flex flex-col gap-8 w-[36%] relative">
+          <div className="flex gap-8 justify-between">
+            <h2 className="text-4xl">Imprimir</h2>
+            <button className="bg-[#FFFFFF11] text-[#999] hover:bg-[#FFFFFF15] hover:text-[#E3E6E8] p-2 rounded-md transition-colors">
+              <ArrowIcon />
+            </button>
+          </div>
+          <p className="text-2xl">
+            <span className="text-sky-400 relative">
+              printf
+              <span className="absolute text-sm -bottom-5 left-0 w-[600px]">
+                imprimir
+              </span>
+            </span>
+            ("
+            <span className="text-red-400 relative">
+              El valor de a es
+              <span className="absolute text-sm -bottom-5 -left-0">texto</span>
+            </span>{" "}
+            <span className="text-yellow-400 relative">
+              %d
+              <span className="absolute text-sm -bottom-5 -left-0">tipo</span>
+            </span>
+            ",{" "}
+            <span className="text-green-400 relative">
+              a
+              <span className="absolute text-sm -bottom-5 -left-0">
+                variable
+              </span>
+            </span>
+            );
+          </p>
+          <p className="text-2xl">
+            <span className="text-sky-400 relative">
+              printf
+              <span className="absolute text-sm -bottom-5 left-0 w-[600px]">
+                imprimir
+              </span>
+            </span>{" "}
+            ("
+            <span className="text-red-400 relative">
+              El valor de b es
+              <span className="absolute text-sm -bottom-5 -left-0">texto</span>
+            </span>{" "}
+            <span className="text-yellow-400 relative">
+              %f
+              <span className="absolute text-sm -bottom-5 -left-0">tipo</span>
+            </span>
+            ",{" "}
+            <span className="text-green-400 relative">
+              b
+              <span className="absolute text-sm -bottom-5 -left-0">
+                variable
+              </span>
+            </span>
+            );
           </p>
         </section>
       </main>
